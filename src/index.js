50a1eac5495c6a177ba2912c20d8b678c1729b6b
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-06-09 03:31:04 +0100

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+
 const todos = [
-    { index: 1, description: 'Learn software development skill', completed: true },
-    { index: 2, description: 'Get a job', completed: false },
-    { index: 3, description: 'Upgrade my system', completed: false },
-  ];
-  
-  const taskContainer = document.querySelector('.body_cont');
-  
-  const tasks = todos.map((todo) => `
+  { index: 1, description: 'Learn software development skill', completed: true },
+  { index: 2, description: 'Get a job', completed: false },
+  { index: 3, description: 'Upgrade my system', completed: false },
+];
+
+const taskContainer = document.querySelector('.body_cont');
+
+const tasks = todos.map((todo) => `
       <div class="todos">
         <div class="task"><input type="checkbox" id="demoCheckbox" name="checkbox" value="1">
             <label for="demoCheckbox">${todo.description}</label>
@@ -16,5 +17,5 @@ const todos = [
         <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
       </div>
       `).join('');
-  
-  taskContainer.innerHTML = tasks;
+
+taskContainer.innerHTML = tasks;
